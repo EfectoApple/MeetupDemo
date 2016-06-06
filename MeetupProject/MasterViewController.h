a410b7609005592c05f61f0752d2de44b656d360
Luis <luisrollongordo@macbook-pro-de-luis.local>
2016-06-06 13:21:11 +0200

    Initial Commit

---
+++ MeetupProject/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MeetupProject
+//
+//  Created by Luis Rollon Gordo on 6/6/16.
+//  Copyright © 2016 Efecto Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
